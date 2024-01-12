1c3e2df75b2e1c0b16900ff1b2cc6568320f42ec
jacobhumston-school <jhumston24@sciototech.org>
2024-01-12 14:26:21 -0500

    dev_printer is now printer.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import dev_printer as p
+import printer as p
 
 e = ["example option", "another example option", "ANOTHER example option", "meow", "lol", "haha", "ok", "AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH", "LOL"]
 i = p.Input.select("please select an option", e, "heyy", hide_instructions=True)
